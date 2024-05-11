36c6b839d2d00edfbbdfa00bcd8ec888541466a8
CesarCouto <cesarrcouto@outlook.com>
2024-05-10 23:34:33 -0300

    configurações de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Footer from './assets/Componentes/Footer'
 
 
 
-
 function App() {
   
 
@@ -15,6 +14,7 @@ function App() {
     <>
       <Header />
       <Home />
+      <Footer />
     </>
   )
 }
