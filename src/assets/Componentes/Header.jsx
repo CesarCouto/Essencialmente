0ecfa35091eb19011d72d236dc55d2e476445f04
CesarCouto <cesarrcouto@outlook.com>
2024-05-12 23:46:11 -0300

    configurações de deploy

--- src/assets/Componentes/Header.jsx
+++ src/assets/Componentes/Header.jsx
@@ -1,38 +1,41 @@
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Styles from './Header.module.css';
+import Logo from '../Imagens/logo.png';
+import Facebook from '../Imagens/facebook.png';
+import Instagram from '../Imagens/instagram.png';
+import Whatsapp from '../Imagens/whatsapp.png';
 
-import Styles from './Header.module.css'
-import Logo from '../Imagens/logo.png'
-import Facebook from '../Imagens/facebook.png'
-import Instagram from '../Imagens/instagram.png'
-import Whatsapp from '../Imagens/whatsapp.png'
+function Header() {
+  return (
+    <div>
+      <header className={Styles.header}>
+        <nav className={Styles.nav_left}>
+          <Link to='/Essencialmente'>
+            <img src={Logo} className={Styles.logo} alt="Logo Essencialmente" />
+          </Link>
+        </nav>
 
-function Header(){
-    return(
-        <div>
-            <header  className={Styles.header}>
-                
-                <nav className={Styles.nav_left}>
-                    <Link to='/Essencialmente'><img src={Logo} className={Styles.logo} alt="Logo Essencialmente" /> </Link> 
-                </nav>
+        <nav className={Styles.nav_link}>
+          <ul>
+            <li className={Styles.linkItem}><Link to='/Essencialmente' className={Styles.link}>Início</Link></li>
+            <li className={Styles.linkItem}>Quem somos</li>
+            <li className={Styles.linkItem}><Link to='/Contato' className={Styles.link}>Contato</Link></li>
+            <li>
+              <button className={Styles.btn_contato}>
+                Whatsapp <img src={Whatsapp} alt="Logo do Whatsapp" />
+              </button>
+            </li>
+          </ul>
+        </nav>
 
-                <nav className={Styles.nav_link}>
-                    <ul>
-                        <li id={Styles.inicio}><Link to='/Essencialmente'>Início</Link></li>
-                        <li>Quem somos</li>
-                        <li><Link to='/Contato'> Contato</Link></li>
-                        <li><button className={Styles.btn_contato}>Whatsapp <img src={Whatsapp} alt="Logo do Whatsapp" /></button></li>
-                    </ul>
-                </nav>
-
-                <nav className={Styles.nav_right}>
-                    <img src={Facebook} className={Styles.redes_sociais} alt="Logo do facebook" />
-                    <img src={Instagram} className={Styles.redes_sociais} alt="Logo do Instagram" />
-                </nav>
-               
-            </header>
-          
-        </div>
-    )
+        <nav className={Styles.nav_right}>
+          <img src={Facebook} className={Styles.redes_sociais} alt="Logo do facebook" />
+          <img src={Instagram} className={Styles.redes_sociais} alt="Logo do Instagram" />
+        </nav>
+      </header>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
