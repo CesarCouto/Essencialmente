3ec8a964b7cbe4b02d3ef3d3910240b2077fdab5
CESAR RODRIGUES COUTO <cesar.rcouto@senacsp.edu.br>
2024-05-14 13:05:27 -0300

    rolamento de pagina

--- src/assets/Componentes/Header.jsx
+++ src/assets/Componentes/Header.jsx
@@ -7,20 +7,29 @@ import Instagram from '../Imagens/instagram.png';
 import Whatsapp from '../Imagens/whatsapp.png';
 
 function Header() {
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "auto"
+    });
+  };
+
+
   return (
     <div>
       <header className={Styles.header}>
         <nav className={Styles.nav_left}>
-          <Link to='/Essencialmente'>
+          <Link to='/Essencialmente' onClick={scrollToTop}>
             <img src={Logo} className={Styles.logo} alt="Logo Essencialmente" />
           </Link>
         </nav>
 
         <nav className={Styles.nav_link}>
           <ul>
-            <li className={Styles.linkItem}><Link to='/Essencialmente' className={Styles.link}>Início</Link></li>
-            <li className={Styles.linkItem}><Link to='/Quem_Somos' className={Styles.link}>Quem somos </Link></li>
-            <li className={Styles.linkItem}><Link to='/Contato' className={Styles.link}>Contato</Link></li>
+            <li className={Styles.linkItem}><Link to='/Essencialmente' className={Styles.link} onClick={scrollToTop}>Início</Link></li>
+            <li className={Styles.linkItem}><Link to='/Quem_Somos' className={Styles.link} onClick={scrollToTop}>Quem somos </Link></li>
+            <li className={Styles.linkItem}><Link to='/Contato' className={Styles.link} onClick={scrollToTop}>Contato</Link></li>
             <li>
               <a href="https://api.whatsapp.com/send?phone=5511916464183&text=Ol%C3%A1%20Paula" target='_blank' rel=''>
                 <button className={Styles.btn_contato}>
--- src/assets/Componentes/Home.jsx
+++ src/assets/Componentes/Home.jsx
@@ -20,6 +20,7 @@ function Home(){
                 </div>
 
             </section>
+            
 
             <Sobre />
             <Competencia />
